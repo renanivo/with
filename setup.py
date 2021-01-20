061131d5003be34f37163da69b469d7e4637be86
Doppins <contact@doppins.com>
2021-01-20 16:32:51 +0000

    Upgrade dependency prompt-toolkit to ~=3.0.11

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     install_requires=[
         'appdirs>=1.4.3',
         'docopt>=0.6.2',
-        'prompt-toolkit~=3.0.10',
+        'prompt-toolkit~=3.0.11',
         'python-slugify>=1.2.3',
     ],
     packages=['withtool'],
